bd8cf4a8255d16bc3b2e9765462d4cbcc5398160
Fred776 <fredfred776@gmail.com>
2022-01-24 00:03:27 -0500

    add imperative handlers to buttons in Main.js to open popups

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,42 @@
 import React from "react";
 
 function Main() {
-    return(
-        <main>
-        <section class="profile page__wrapper">
-          <button class="profile__avatar" type="button" aria-label="edit-avatar">
-            <img class="profile__avatar-button" alt="edit-avatar" />
-          </button>
-          <div class="profile__info">
-            <div class="profile__info-name">
-              <h1 class="profile__name"></h1>
-              <button class="profile__edit-button" type="button" aria-label="edit-profile"></button>
-            </div>
-            <p class="profile__profession"></p>
+  function handleEditAvatarClick() {
+    const editAvatarPopup = document.querySelector('.modal_type_edit-avatar');
+    editAvatarPopup.classList.add('modal_open');
+  }
+
+  function handleEditProfileClick() {
+    const editProfilePopup = document.querySelector('.modal_type_edit');
+    editProfilePopup.classList.add('modal_open');
+  }
+
+  function handleAddPlaceClick() {
+    const addCardPopup = document.querySelector('.modal_type_add');
+    addCardPopup.classList.add('modal_open');
+  }
+
+  return(
+    <main>
+      <section class="profile page__wrapper">
+        <button class="profile__avatar" type="button" aria-label="edit-avatar">
+          <img class="profile__avatar-button" onClick={handleEditAvatarClick} alt="edit-avatar" />
+        </button>
+        <div class="profile__info">
+          <div class="profile__info-name">
+            <h1 class="profile__name"></h1>
+            <button class="profile__edit-button" type="button" onClick={handleEditProfileClick} aria-label="edit-profile"></button>
           </div>
-          <button class="profile__add-button" type="button" aria-label="add-profile"></button>
-        </section>
-        <section class="places page__wrapper">
-          <ul class="places__list">
-          </ul>
-        </section>
-      </main>
-    );
+          <p class="profile__profession"></p>
+        </div>
+        <button class="profile__add-button" type="button" onClick={handleAddPlaceClick} aria-label="add-profile"></button>
+      </section>
+      <section class="places page__wrapper">
+        <ul class="places__list">
+        </ul>
+      </section>
+    </main>
+  );
 }
 
 export default Main;
