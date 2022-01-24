d96f9727654e7b22854c3f8f73814cc95be4470f
Fred776 <fredfred776@gmail.com>
2022-01-23 22:57:14 -0500

    import and add logo in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-
+import logo from './images/header__logo.svg';
 
 function App() {
   return (
     <div class="page">
       <header class="header page__wrapper page__wrapper_disabled">
-        <img class="header__logo" alt="Around the US" />
+        <img className='header__logo' src={logo} alt="Around the US" />
       </header>
       <main>
         <section class="profile page__wrapper">
