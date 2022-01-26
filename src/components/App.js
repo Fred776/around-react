2a9ef79322c6c1851ec6f5102392636eb7030d29
Fred776 <fredfred776@gmail.com>
2022-01-26 14:21:23 -0500

    create user data state and effect using api

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Footer from './Footer';
 import PopupWithForm from "./PopupWithForm";
 
 function App() {
+  // Popup State and Event handlers //
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,33 @@
 import React from "react";
+import api from '../utils/api.js';
+import editButtonSign from '../images/profile__edit-button-sign.svg';
 
 function Main({onEditProfileClick, onAddPlaceClick, onEditAvatarClick, onCardClick}) {
+  // User data State and Effects //
+  const [userName, setUserName] = React.useState('');
+  const [userDescription, setUserDescription] = React.useState('');
+  const [userAvatar, setUserAvatar] = React.useState('');
+
+  React.useEffect(() => {
+    api.getUserInfo().then(data => {
+      setUserName(data.name);
+      setUserDescription(data.about);
+      setUserAvatar(data.avatar);
+    })
+  }, []) 
+
   return(
     <main>
       <section className="profile page__wrapper">
-        <button className="profile__avatar" type="button" aria-label="edit-avatar">
-          <img className="profile__avatar-button" onClick={onEditAvatarClick} alt="edit-avatar" />
+        <button className="profile__avatar" type="button" style={{backgroundImage: `url(${userAvatar})`}} onClick={onEditAvatarClick} aria-label="edit-avatar">
+          <img className="profile__avatar-button" src={editButtonSign} alt="edit-avatar" />
         </button>
         <div className="profile__info">
           <div className="profile__info-name">
-            <h1 className="profile__name"></h1>
+            <h1 className="profile__name">{userName}</h1>
             <button className="profile__edit-button" type="button" onClick={onEditProfileClick} aria-label="edit-profile"></button>
           </div>
-          <p className="profile__profession"></p>
+          <p className="profile__profession">{userDescription}</p>
         </div>
         <button className="profile__add-button" type="button" onClick={onAddPlaceClick} aria-label="add-profile"></button>
       </section>
