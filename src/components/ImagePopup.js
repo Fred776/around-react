0ee764a6009874ede17f13fa1ab32490f728926d
Fred776 <fredfred776@gmail.com>
2022-01-27 16:52:54 -0500

    replace ternary operator with && call

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function ImagePopup({card, onClose}) {
   return(
-    <div className={`modal modal_type_image-preview" ${card ? 'modal_open' : ''}`}>
+    <div className={`modal modal_type_image-preview" ${Boolean(card) && 'modal_open'}`}>
       <figure className="modal__figure">
         <img className="modal__image" alt="place" src={card.link} />
         <figcaption className="modal__caption">{card.name}</figcaption>
