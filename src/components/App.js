dc6cc249b6a0071dd89289533528af82a885fb19
Fred776 <fredfred776@gmail.com>
2022-01-28 00:36:12 -0500

    set selectedCard original state value to null and update ImagePopup component accordingly

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
-  const [selectedCard, setSelectedCard] = React.useState('');
+  const [selectedCard, setSelectedCard] = React.useState(null);
 
   function handleEditProfileClick() {
     setIsEditProfilePopupOpen(true);
@@ -33,7 +33,7 @@ function App() {
     setIsEditProfilePopupOpen(false);
     setIsAddPlacePopupOpen(false);
     setIsEditAvatarPopupOpen(false);
-    setSelectedCard({});
+    setSelectedCard(null);
   }
 
   return (
--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function ImagePopup({card, onClose}) {
   return(
-    <div className={`modal modal_type_image-preview" ${Boolean(card) && 'modal_open'}`}>
+    <div className={`modal modal_type_image-preview" ${card ? 'modal_open' : ''}`}>
       <figure className="modal__figure">
-        <img className="modal__image" alt="place" src={`${card.link}`} />
-        <figcaption className="modal__caption">{card.name}</figcaption>
+        <img className="modal__image" alt="place" src={card ? card.link : ''}/>
+        <figcaption className="modal__caption">{card ? card.name : ''}</figcaption>
         <button className="modal__close-button modal__close-button_type_image" type="button" onClick={onClose}  aria-label="close"/>
       </figure> 
     </div>
