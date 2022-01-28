3eaa1a0d4781ec8da1dcbe3d12d5ccd78429f3d2
Fred776 <fredfred776@gmail.com>
2022-01-28 00:14:38 -0500

    fix img src link in ImagePopup.js

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -4,7 +4,7 @@ function ImagePopup({card, onClose}) {
   return(
     <div className={`modal modal_type_image-preview" ${Boolean(card) && 'modal_open'}`}>
       <figure className="modal__figure">
-        <img className="modal__image" alt="place" src={card.link} />
+        <img className="modal__image" alt="place" src={`${card.link}`} />
         <figcaption className="modal__caption">{card.name}</figcaption>
         <button className="modal__close-button modal__close-button_type_image" type="button" onClick={onClose}  aria-label="close"/>
       </figure> 
