325809b3d6c2495e1d57a8262f67ff7ce354e061
Fred776 <fredfred776@gmail.com>
2022-02-09 15:20:34 -0500

    add card.name to img alt attribute

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -5,7 +5,7 @@ function ImagePopup({card, onClose}) {
   return (
     <div className={`modal modal_type_image-preview" ${card ? 'modal_open' : ''}`}>
       <figure className="modal__figure">
-        <img className="modal__image" alt="place" src={card ? card.link : ''}/>
+        <img className="modal__image" alt={card ? card.name : ''} src={card ? card.link : ''}/>
         <figcaption className="modal__caption">{card ? card.name : ''}</figcaption>
         <button className="modal__close-button modal__close-button_type_image" type="button" onClick={onClose}  aria-label="close"/>
       </figure> 
